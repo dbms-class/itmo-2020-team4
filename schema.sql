e098edd6d8fe48bde852ee76e2646bee6bb8942b
Kirill Bochkarev <bochkarev.cyril@gmail.com>
2020-10-28 13:45:05 +0000

    More improvements

--- schema.sql
+++ schema.sql
@@ -10,17 +10,29 @@ drop table if exists Volunteer;
 drop table if exists Delegation;
 drop table if exists Facility;
 drop table if exists Address;
-
+drop table if exists Sport;
 
 drop type if exists Medal;
-drop type if exists Sport;
+drop type if exists FacilityFunction;
 --end clean
 
 create type Medal as enum('gold', 'silver', 'bronze');
--- Dict for Sports? Sports must have n > 0 competitions each
-create type Sport as enum('basketball', 'tennis', 'football', 'swimming');
+create type FacilityFunction as enum('restaurant', 'household', 'pool', 'track',
+'stadium', 'pitch');
 
--- create sequence card_number;
+-- Dict for Sports
+create table Sport
+(
+    id              serial primary key,
+    name            text unique not null
+);
+
+-- Dict for Countries that are coming to the event
+create table Country
+(
+    id              serial primary key,
+    name            text unique not null
+);
 
 -- у каждого объекта есть адрес (название улицы в деревнеи номер дома)
 create table Address
@@ -29,11 +41,12 @@ create table Address
 
     street_name     text not null,  -- ?
     house_number    int not null,
+    additional      text, -- if there are corps and so on
 
-    unique (street_name, house_number)
+    unique (street_name, house_number, additional)
 );
 
--- у каждого объекта есть адрес, функци. предн. и опц. собственное имя
+-- у каждого объекта есть адрес, функц. предн. и опц. собственное имя
 create table Facility
 (
     id              serial primary key,
@@ -51,54 +64,55 @@ create table Delegation
     id              serial primary key,
 
     headquarters    int references Facility not null,
+    country_id      int references Country not null,
 
-    county          text not null, -- must reference some country? countries dict?
     director_name   text not null,
-    director_phone  text unique not null check(director_phone ~ E'^\\+\\d{11,15}')
+    director_phone  text unique not null check(director_phone ~ E'^\\+\\d{11,15}') -- can be used as key?
 );
 
 -- у каждым спортсмена не уникальный волонтёр(имя,телефон,карточка как у спортсмена)
 create table Volunteer
 (
     -- card_number serial primary key default nextval('card_number'),
-    card_number serial primary key check(card_number > 1e6),
+    card_number     serial primary key check(card_number >= 1e6),
 
     name            text not null,
     phone_number    text not null check(phone_number ~ E'^\\+\\d{11,15}') 
 );
+-- У спортсменов карточки до миллиона, у волонтёров после
+alter sequence volunteer_card_number_seq restart with 1000000;
 
 -- имя, пол, рост, вес, возраст, нац. делегация, где-то живёт
 create table Sportsman
 (
     card_number   serial primary key check(card_number < 1e6),
 
-    delegation_id int references Delegation,
-    residence     int references Facility,
-    helper        int references Volunteer,
+    delegation_id int references Delegation not null,
+    facility_id   int references Facility not null,
+    volunteer_id  int references Volunteer not null,
 
     name          text not null,  -- TODO
     gender        bool not null,
     height        float not null,
     weight        float not null,
-    age           int not null check(age < 99 AND age > 14),
-    sports_n      int check(sports_n > 0) -- smth like this, sportsman has to participate in smth
+    age           int not null check(age < 99 and age > 14)
 );
 
 --Каждый спортсмен выступает в каком-то виде спорта, возможно даже не в одном
 create table SportParticipation
 (
-    sportsman_card int references Sportsman,
-    sport_id      Sport, 
+    sportsman_card int references Sportsman not null,
+    sport_id       int references Sport not null, 
 
     unique (sportsman_card, sport_id)
 );
 
 -- некоторые объекты могут быть проассоциированы с некоторым множеством видов спорта, которые в них проходят
-create table SportFacility
+create table SportFacility -- sport - facility relation
 (
     id              serial primary key,
-    facility        int references Facility,
-    sport_id        Sport,
+    facility        int references Facility not null,
+    sport_id        int references Sport not null, 
 
     unique (facility, sport_id)
 );
@@ -108,24 +122,41 @@ create table SportFacility
 create table Competition
 (
     id                  serial primary key,
-    -- competition_type_id int references CompetitionType, -- enum or smth for type?
-    place               int references SportFacility,
+    place               int references SportFacility not null,
 
-    name                text,
-    time_               timestamp,
-    -- prizes              bool, -- state that medals can be won
-    -- How to deal with participating sportsmen???
+    name                text not null, -- Competition type like swim 200m or run 100 m
+    time_               timestamp not null,
+    description         text not null, -- Women/Men Final, Semi, Quarters, top-100
+    -- medals              bool not null, -- state that medals can be won
 
-    unique (time_, place)
+    unique (time_, place),
+    unique (name, description)
 );
 
-create table MedalHolder
+create table MedalHolders
 (
-    sportsman_card int references Sportsman,
-    competition_id int references Competition,
+    competition_id      int references Competition not null,
+    gold_holder         int references Sportsman not null,
+    silver_holder       int references Sportsman not null,
+    bronze_holder       int references Sportsman not null,
 
-    unique (sportsman_card, competition_id)
-    -- check that medals were given for that competition???
+    unique (competition_id)
+);
+
+alter table MedalHolders
+add constraint chk_dif_holders 
+check (gold_card != silver_card and silver_card != bronze_card and bronze_card != gold_card);
+
+create table CompetitionParticipation
+(
+    sportsman_card      int references Sportsman not null,
+    competition_id      int references Competition not null,
+
+    time_               timestamp not null, -- we leave time here so
+    -- How to deal with participating sportsmen???
+
+    unique (sportsman_card, competition_id),
+    unique (sportsman_card, time_)
 );
 
 -- транспортное средство (регистрационный номер и вместимость)
@@ -133,7 +164,7 @@ create table Transport
 (
     -- serial_number   serial primary key, -- is it necessary if we have registration?
     registration    text primary key, -- check with regex?
-    capacity        int check (capacity >= 0)
+    capacity        int not null check (capacity >= 0)
 );
 
 --Волонтёрам дают задания (дата, время, текстовое описание, может быть ТС)
@@ -144,11 +175,25 @@ create table VolunteerTask
     volunteer_id    int references Volunteer not null,
     transport_reg   text references Transport,
 
-    time_           timestamp,
-    description     text,
+    time_           timestamp not null,
+    description     text not null,
 
     unique (time_, volunteer_id)
 );
 
 -- Fill
+INSERT INTO Sport (name)
+SELECT unnest(ARRAY['swimming', 'running', 'football', 'basketball', 'tennis', 'poker', 'chess']);
+
+WITH Names AS (
+  SELECT unnest(ARRAY[
+      'Pooshkina', 'Kolotooshkina', 'Lenina', 'Uncanny'
+  ]) AS name
+)
+INSERT INTO Address(street_name, house_number)
+SELECT name, (3+random()*20)::INT
+FROM Names;
+
 INSERT INTO Transport (registration, capacity) VALUES ('A321TT179',3);
+
+INSERT INTO Volunteer (name, phone_number) VALUES ('SomeVolunteer','+4546667772288');
